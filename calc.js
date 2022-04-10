093dbabe3df4066e48666e142900579467d3a86a
KPOJI <99410092+KPOJI@users.noreply.github.com>
2022-04-10 10:52:29 +0300

    доработка двойного нажатия на вычислит. кнопки

--- calc.js
+++ calc.js
@@ -15,28 +15,30 @@ for (let i=0;i<numbers.length;i++) {
     
     
         numbers[i].addEventListener('click', function(){
-            console.log(output.textContent.length)
-            if (par1.length<9) {
+            console.log('dlina= ' + par1.toString().length, 'a = '+par1)
+            if (par1.toString().length<9) {
         if (result) {result=0; output.textContent='';}
         if (par1===''){output.textContent=''}
         if(!(output.textContent==='0')){output.textContent+=numbers[i].value;}
         par1= output.textContent ;
     }
         
-    console.log(par1,par2)  }) 
+    console.log('a = ' + par1, 'b = ' + par2, 'result = '+result)  }) 
     
         
 }
 
 for (let i = 0; i < calculateButtons.length; i++) {
     calculateButtons[i].addEventListener('click', function(){
-
+        if (par1!=='') {
+            
+        
             par2=par1;
             par1='';
-            console.log(par1,par2);
+            console.log('a = '+par1,'b = '+par2);
             a=calculateButtons[i].value;
             
-            
+        }    
                 })
 
 }
@@ -52,7 +54,8 @@ res.addEventListener('click', function(){
     if(output.textContent==='') {result=''}
    
     else{output.textContent=result;}
-    par1=result})
+    par1=result;
+    console.log('result = '+result, 'a = '+par1,'b = '+par2);})
 
 clear.addEventListener('click', function(){
     result='';
@@ -60,5 +63,7 @@ clear.addEventListener('click', function(){
     par1='';
     par2='';
     output.textContent='';
+
+
     
 })
