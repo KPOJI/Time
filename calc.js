7d28fd4f023cfa31575b84cd56af73b865842e19
KPOJI <99410092+KPOJI@users.noreply.github.com>
2022-04-12 14:57:18 +0300

    добавление точки

--- calc.js
+++ calc.js
@@ -9,61 +9,51 @@ let result = '';
 let calculateButtons = document.querySelectorAll('.calculateButton');
 let a;
 
-output.textContent='';
+output.textContent = '';
 
-for (let i=0;i<numbers.length;i++) {
-    
-    
-        numbers[i].addEventListener('click', function(){
-            console.log('dlina= ' + par1.toString().length, 'a = '+par1)
-            if (par1.toString().length<9) {
-        if (result) {result=0; output.textContent='';}
-        if (par1===''){output.textContent=''}
-        if(!(output.textContent==='0')){output.textContent+=numbers[i].value;}
-        par1= output.textContent ;
-    }
-        
-    console.log('a = ' + par1, 'b = ' + par2, 'result = '+result)  }) 
-    
-        
+for (let i = 0; i < numbers.length; i++) {
+
+
+	numbers[i].addEventListener('click', function () {
+		console.log('dlina= ' + par1.toString().length, 'a = ' + par1)
+		if (par1.toString().length < 9) {
+			if (result) { result = 0; output.textContent = ''; }
+			if (par1 === '') { output.textContent = '' }
+			if (!(output.textContent === '0')) { output.textContent += numbers[i].value; }
+			par1 = output.textContent;
+		}
+		console.log('a = ' + par1, 'b = ' + par2, 'result = ' + result)
+	})
 }
 
 for (let i = 0; i < calculateButtons.length; i++) {
-    calculateButtons[i].addEventListener('click', function(){
-        if (par1!=='') {
-            
-        
-            par2=par1;
-            par1='';
-            console.log('a = '+par1,'b = '+par2);
-            a=calculateButtons[i].value;
-            
-        }    
-                })
-
+	calculateButtons[i].addEventListener('click', function () {
+		if (par1 !== '') {
+			par2 = par1;
+			par1 = '';
+			console.log('a = ' + par1, 'b = ' + par2);
+			a = calculateButtons[i].value;
+		}
+	})
 }
 console.log(a);
 
+res.addEventListener('click', function () {
+	if (a === '+') { result = Number(par1) + Number(par2) };
+	if (a === '-') { result = Number(par2) - Number(par1) };
+	if (a === '*') { result = Number(par2) * Number(par1) };
+	if (a === '/') { result = Number(par2) / Number(par1) };
+	if (!a) { result = par1 }
+	if (output.textContent === '') { result = '' }
+	else { output.textContent = result; }
+	par1 = result;
+	console.log('result = ' + result, 'a = ' + par1, 'b = ' + par2);
+})
 
-res.addEventListener('click', function(){
-    if (a==='+') {result=Number(par1) + Number(par2)};
-    if (a==='-') {result=Number(par2) - Number(par1)};
-    if (a==='*'){result=Number(par2) * Number(par1)};
-    if (a==='/') {result=Number(par2) / Number(par1)};
-    if (!a) {result=par1}
-    if(output.textContent==='') {result=''}
-   
-    else{output.textContent=result;}
-    par1=result;
-    console.log('result = '+result, 'a = '+par1,'b = '+par2);})
-
-clear.addEventListener('click', function(){
-    result='';
-    a='';
-    par1='';
-    par2='';
-    output.textContent='';
-
-
-    
+clear.addEventListener('click', function () {
+	result = '';
+	a = '';
+	par1 = '';
+	par2 = '';
+	output.textContent = '';
 })
