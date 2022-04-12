e42f1a21fdd35098a71813a0323671a655604208
KPOJI <99410092+KPOJI@users.noreply.github.com>
2022-04-13 00:05:13 +0300

    доработка работы точки с минусом и нулем

--- calc.js
+++ calc.js
@@ -17,55 +17,80 @@ for (let i = 0; i < numbers.length; i++) {
 		numValue = numbers[i].value;
 		out = output.textContent;
 		console.log('out   ===   ' + out);
-		console.log('dlina= ' + par1.toString().length, 'a = ' + par1)
+		
 		if (par1.toString().length < 9) {
 
-			if (result) { result = 0; out = ''; }
+			
 			if ((par1 === '') && !(out === '-')) { out = '' }
 
 			if ((out === '') && numValue === '.') { out = '0.' }
-			if ((out === '0') && !(numValue === '.')) { out = '' }
-			if (out.includes('.') && (numValue === '.')) { numValue = '' }
+			if ((out === '-') && numValue === '.') { out = '-0.' }
+
+			if (((out === '0')||(out === '-0')) && !(numValue === '.')) { out = '0' }
 			
-			 if (!(out === '0')||(out === '-')) { out += numValue; }
-			if ((out === '0') && (numValue === '.')) { out += numValue; }
+			if (out.includes('.') && (numValue === '.')) { numValue = '' }
+			if(result&&par2===''&&numValue === '.') {out='0'; par2='0';result='';a=''}
+			if(result&&par2==='') {out=''; par2='0';result='';a=''}//?
+			//убрать out=''?
+			if (result) { out += numValue; }   
+			else{ if (!(out === '0')||(out === '-')) { out += numValue; }
+			if ((out === '0') && (numValue === '.')) { out += numValue; }}
 
 
 
 			par1 = out;
 
 		}
-		console.log('a = ' + par1, 'b = ' + par2, 'result = ' + result)
+		console.log('out = '+out,'a = ' + par1, 'b = ' + par2, 'result = ' + result, 'ZNAK', a)
 		output.textContent = out;
 	})
 
 }
 for (let i = 0; i < calculateButtons.length; i++) {
 	calculateButtons[i].addEventListener('click', function () {
-		if (output.textContent === '') { output.textContent = '-'; }
+		a = calculateButtons[i].value;
+		
+		if (output.textContent === ''&&a==='-') { output.textContent = '-'; }
 		else if (par1 !== '') {
 			par2 = par1;
 			par1 = '';
 			console.log('a = ' + par1, 'b = ' + par2);
-			a = calculateButtons[i].value;
+			
 		}
+		
 	})
 }
 console.log(a);
+
+
 res.addEventListener('click', function () {
-	if (a === '+') { result = Number(par1) + Number(par2) };
-	if (a === '-') { result = Number(par2) - Number(par1) };
-	if (a === '*') { result = Number(par2) * Number(par1) };
-	if (a === '/') { result = Number(par2) / Number(par1) };
+	console.log(a);
+	console.log(result);
+	if (a === '+'&&(result||result===0)) { result += Number(par1)}
+	else if (a === '+') { result = Number(par1) + Number(par2) };
+	
+	if (a === '-'&&(result||result===0)) { result -= Number(par1)}
+	else if ((a === '-')&&(par2)) { result = Number(par2) - Number(par1) };
+
+	if (a === '*'&& (result||result===0)) { result *= Number(par1)}
+	else if (a === '*') { result = Number(par2) * Number(par1) };
+
+	if (a === '/'&&(result||result===0)) { result /= Number(par1)}
+	else if (a === '/') { result = Number(par2) / Number(par1) };
+
+
 	if (!a) { result = par2 }
 	if (output.textContent === '') { result = '' }
 	else { output.textContent = +result.toFixed(8); }
 	if ((output.textContent > 999999999)||(output.textContent < (-999999999)))
 	 { output.textContent = result.toExponential(4); }
-
-	par2 = +result.toFixed(8);
-	console.log('result = ' + result, 'a = ' + par1, 'b = ' + par2);
+	//par2 = +result.toFixed(8);
+	par2=''
+	console.log('out = '+out,'result = ' + result, 'a = ' + par1, 'b = ' + par2 , 'ZNAK'+a);
+	
 })
+
+
 clear.addEventListener('click', function () {
 	result = '';
 	a = '';
